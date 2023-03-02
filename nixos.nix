22c8a50f270bf3cdd06982aea385318a40b0e539
Bogdan Manole <manoleb@gmail.com>
2023-03-02 20:23:47 +0000

    adding nixos.nix

---
+++ nixos.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  common = {
+    inherit config pkgs;
+  };
+  frioHome = import ./nixpkgs/home.frio.nix common;
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  home-manager.users.horus = frioHome;
+
+  #home-manager.users.horus = {
+  #  /* The home.stateVersion option does not have a default and must be set */
+  #  home.stateVersion = "21.03";
+  #  /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+  #};
+}
